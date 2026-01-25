e5169aa85289947dde73477b59e0e1e55b1fbeac
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2026-01-25 09:48:23 +0200

    refactor: sanitation sweep kill jsx and standardize 3-pane layout

--- apps/web/src/components/logo/index.tsx
+++ apps/web/src/components/logo/index.tsx
@@ -1,46 +1,31 @@
 'use client';
+import PropTypes from 'prop-types';
 
 // @next
 import NextLink from 'next/link';
 
 // @mui
-import { useTheme, SxProps, Theme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import ButtonBase from '@mui/material/ButtonBase';
 
 // @project
-// @ts-expect-error - legacy SaasAble component
 import LogoMain from './LogoMain';
-// @ts-expect-error - legacy SaasAble component
 import LogoIcon from './LogoIcon';
 import { APP_DEFAULT_PATH } from '@/config';
 import { generateFocusStyle } from '@/utils/generateFocusStyle';
 
-/***************************  LOGO SECTION - TYPES  ***************************/
-
-interface Props {
-  isIcon?: boolean;
-  sx?: SxProps<Theme>;
-  to?: string;
-}
-
 /***************************  MAIN - LOGO  ***************************/
 
-export default function LogoSection({ isIcon, sx, to }: Props) {
+export default function LogoSection({ isIcon, sx, to }) {
   const theme = useTheme();
 
   return (
-    <NextLink href={!to ? APP_DEFAULT_PATH : to} passHref legacyBehavior>
-      <ButtonBase 
-        disableRipple 
-        sx={{ 
-          ...sx, 
-          // @ts-expect-error - legacy SaasAble component
-          '&:focus-visible': generateFocusStyle(theme.palette.primary.main) 
-        }} 
-        aria-label="logo"
-      >
+    <NextLink href={!to ? APP_DEFAULT_PATH : to} passHref>
+      <ButtonBase disableRipple sx={{ ...sx, '&:focus-visible': generateFocusStyle(theme.vars.palette.primary.main) }} aria-label="logo">
         {isIcon ? <LogoIcon /> : <LogoMain />}
       </ButtonBase>
     </NextLink>
   );
 }
+
+LogoSection.propTypes = { isIcon: PropTypes.bool, sx: PropTypes.any, to: PropTypes.string };
